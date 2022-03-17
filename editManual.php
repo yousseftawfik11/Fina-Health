6f749f5bd24a89f4ecd932d994a33636184be2a9
Hawwa11 <hawwazohair@gmail.com>
2022-03-18 04:26:54 +0800

    commit

--- editManual.php
+++ editManual.php
@@ -9,7 +9,7 @@
     <style> .left {margin-left:43%;text-align:left;margin-top:2%;margin-bottom:2%} </style>
 
     <!--Font Awesome 4 CDN-->
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
     table {
   font-family: arial, sans-serif;
@@ -43,101 +43,57 @@ for($i=0;$i<count($fileData);$i++){
 
 }
 
-//scrub DATA
-// echo "<br>";
-// echo $batchID;
-// $boolBID = scrubBatchID($batchID) . "</b>";
-// echo "&ensp;<b>" .$boolBID;
-
-
-// echo "</br>";
-// echo $deliveyDate;
-// echo "&ensp;<b>" .$boolDd = scrubDeliveryDate($deliveyDate). "</b>";
-
-// for($i = 0; $i < count($itemNo); $i++)
-// {
-//     echo "</br>";
-//     echo $itemNo[$i];
-
-//     $result = scrubNumericData($itemNo[$i]);
-
-//     if($result == 1){
-//         $verify = 1;
-//     }else if($result == 0){
-
-//     }else $itemNo[$i] =$result;
-
-//     echo "<br>";
-//     echo $itemName[$i];
-
-//     $result =  scrubStringData($itemName[$i]);
-
-//     if($result == 1){
-//         $verify = 1;
-//     }else if($result == 0){
-
-//     }else $itemName[$i] =$result;
-
-//     echo "<br>";
-//     echo $quantity[$i];
-
-//     $result = scrubNumericData($quantity[$i]);
-
-//     if($result == 1){
-//         $verify = 1;
-//     }else if($result == 0){
-
-//     }else $quantity[$i] =$result;
-
-//     echo "<br>";
-//     echo $price[$i];
-
-//     $result =  scrubNumericData($price[$i]);
-
-//     if($result == 1){
-//         $verify = 1;
-//     }else if($result == 0){
-
-//     }else $price[$i] =$result;
-
-// }
-
-
-// echo "<br>";
-// echo $totalPrice;
-// echo "&ensp;<b>" . $boolTp = scrubNumericData($totalPrice). "</b>";
-// echo "<br>";
-
-
 
 function showEditTable($batchID, $deliveyDate, $itemNo, $itemName, $quantity, $price, $totalPrice)
 {
+    $_SESSION['batchID'] = $batchID;
+    $_SESSION['delivery_date'] = $deliveyDate;
+    $_SESSION['totalPrice'] = $totalPrice;
+  
+    $_SESSION['itemNo'] = $itemNo;
+    $_SESSION['itemName'] = $itemName;
+    $_SESSION['quantity'] = $quantity;
+    $_SESSION['price'] = $price;
+
     ?>
      <div class="table-responsive">
     <table id="editable_table" class="table table-bordered table-striped">
   
+    <?php
+    echo "<tr><th>Batch Id: ".$batchID  . "</th>";
+
+
+    echo "<th>Delivery Date: ".$deliveyDate . "&ensp;</th>";
+   
+
     
+    echo "<th>Total Price: RM" .$totalPrice . "</th>";
+?>
+
+    <th>
+</th>
+<th>
+    <form action="formUpdate.php" method="post">
+                   <div class="col-md-9">
+                <input type="submit" name="editHeader" value="EDIT" class="btn" />
+                 </div>
+
+                 </form>
+    </th></tr>
+  
     
      <tr>
      <th>Item Id</th>
- <th>Item Name</th>
-      <th>Quantity</th>
-      <th>Price</th>
+     <th>Item Name</th>
+     <th>Quantity</th>
+     <th>Price</th>
+     <th></th>
 
 </tr>
      
      <tbody>
          <?php
-    echo "Batch Id: ".$batchID  . "&ensp;";
-    echo "<br>";
-
-    echo "Delivery Date: ".$deliveyDate . "&ensp;";
-    echo "<br>";
-
-    echo "<br>";
-    echo "Total Price: RM" .$totalPrice;
-    echo "<br>";
-
+    
     for($i = 0; $i < count($itemNo); $i++){
         ?>
         <tr>
@@ -157,56 +113,47 @@ function showEditTable($batchID, $deliveyDate, $itemNo, $itemName, $quantity, $p
             echo $quantity[$i];
             
            
-            ?></td>
+            ?>
+            </td>
             <td><?php
             echo$price[$i];
+
+            ?>
+                 <td>
+
+                 <form action="formUpdate.php" method="post">
+                   <div class="col-md-9">
+                <input type="submit" name="editItem" value="EDIT" class="btn" />
+                 </div>
+
+                 </form>
+                
+                 </td>
+    
+    
+    <?php
                
            
       }
 
      ?></td>
+
 </tr>
+  
 
 <!-- all i added -->
 <tbody>
+
     </table>
+    
     <br>
-    <div class="col-md-9">
- <button onclick="clickMe()">Upload DB</button>
-       </div>
+   
      <?php
 }
 
-function dBupload()
-{
-    echo " uploaddd" ; 
-}
+
 
 ?>
 
-<script>
-$(document).ready(function(){  
-     $('#editable_table').Tabledit({
-      url:'GR_action.php',
-      columns:{
-       identifier:[0, "GR_id"],
-       editable:[[1, 'DN_code'], [2, 'OR_code'],[3,'GR_rates'],
-    [4,'GR_airline'], [5,'GR_Wbreak'],[6,'GR_type'],[7,'FSC_rate'],[8,'SSC_rate'],[9,'GR_promotion']]
-      },
-      restoreButton:false,
-      onSuccess:function(data, textStatus, jqXHR)
-      {
-       if(data.action == 'delete')
-       {
-        $('#'+data.itemNo).remove();
-       }
-      }
-     });
-});  
 
-function clickMe(){
-var result ="<?php php_func(); ?>"
-document.write(result);
-}
 
- </script>

50a8f21da23f43baf2d859c33dc68e19ad74f3cd
AbdelrahmanOsama99 <bodaosa@yahoo.com>
2022-03-17 05:13:16 +0300

    css for IC

--- iCHome.php
+++ iCHome.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!--Stylesheet-->
+    <link rel="stylesheet" href="css/iCStyles.css">
+
     <!--Bootstrap 5-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
@@ -14,12 +17,43 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Homepage</title>
 
+    <!---->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+
 </head>
 <body>
     <div class="navWrapper">
         <div class="sideNav"><!--Side Navbar-->
             <ul>
                 <a class="navbar-brand" href="iCHome.php">FINA Health Care</a>
+                <br><br><br>
+                <li class="active"><a href="iCHome.php"><i class="fa fa-pie-chart"> Dashboard</i></a></li>
+                <li><a href="#"><i class="fa fa-book"> Items</i></a></li>
+                <li><a href="#"><i class="fa fa-envelope"> Stock Report</i></a></li>
             </ul>
         </div>
         <div class="container">
@@ -56,7 +90,7 @@
                             Pie Graph of Current Stock Level
                         </div>
                         <div class="card-body">
-                            <!--Insert Graph-->
+                            <div id="piechart" style="width: 550px; height: 400px;"></div><!--Insert Graph-->
                         </div>
                     </div>
                 </div>

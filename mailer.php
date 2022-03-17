4c9659da98e0af36e1cd06437d8c7b7e35a69673
yousseftawfik11 <67921054+yousseftawfik11@users.noreply.github.com>
2022-03-17 11:13:29 +0200

    mailer

---
+++ mailer.php
@@ -0,0 +1,57 @@
+<?php
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+
+
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+
+try {
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'tornadoteam.website';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'announcements@tornadoteam.website';                     //SMTP username
+    $mail->Password   = 'Ymtk_Ymtk11';                               //SMTP password
+   // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('announcements@tornadoteam.website', 'Tornado Team');
+
+        $mail->addAddress('finahealthcare123@gmail.com', 'Weekly Report');     //Add a recipient
+
+    
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $uploaddri='attachments/';
+    $Fname='10.pdf';
+    $mail->addAttachment($uploaddri,$Fname);         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Stock';
+    $mail->Body    = 'This is the weekly report regarding stocks status';
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+
+?>

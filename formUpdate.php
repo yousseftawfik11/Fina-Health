ffa6b2d840f3218f31f4b2edb29cd41aa03460c8
Hawwa11 <hawwazohair@gmail.com>
2022-03-18 08:47:41 +0800

    edit manual

--- formUpdate.php
+++ formUpdate.php
@@ -1,5 +1,15 @@
 <?php
 
+if (isset($_POST["editBatch"])) {
+    $batchID = $_SESSION['batchID'];
+    $deliveyDate = $_SESSION['delivery_date'];
+    $totalPrice = $_SESSION['totalPrice'];
+    $itemNo = $_SESSION['itemNo'];
+    $itemName = $_SESSION['itemName'];
+    $quantity = $_SESSION['quantity'];
+    $price = $_SESSION['price'];
 
+    
+}
 
 ?>
